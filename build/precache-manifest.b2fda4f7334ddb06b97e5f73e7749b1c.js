ee166b1d5b7b9702b9fc53c57d5076dc1fa74d49
PaulGOUX27 <paul.goux@wanadoo.fr>
2020-09-24 11:24:04 +0300

    uibuild

---
+++ build/precache-manifest.b2fda4f7334ddb06b97e5f73e7749b1c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b1f9ec11c8504c0f029d923de1d9d23",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ef828c405e1fd964b058",
+    "url": "/static/css/main.9b1e4320.chunk.css"
+  },
+  {
+    "revision": "2469ec848eef6bb55628",
+    "url": "/static/js/2.90705a08.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.90705a08.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef828c405e1fd964b058",
+    "url": "/static/js/main.8cdb25cf.chunk.js"
+  },
+  {
+    "revision": "f4d2ac87a76b6331fd91",
+    "url": "/static/js/runtime-main.4234e0d5.js"
+  }
+]);
